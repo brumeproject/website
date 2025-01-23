f176a82b375027e6a706134576352a1a33b9246b
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-01-24 00:20:21 +0100

    d

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,11 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="antialiased">
+      <body className="antialiased"
+        data-theme="light">
         <Main />
         <NextScript />
       </body>
